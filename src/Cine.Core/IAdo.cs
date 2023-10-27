c769124b60ed0e45b23d5b632bab86ba0433b533
JuanMerida <juan.meridaet12d1@gmail.com>
2023-10-27 09:08:11 -0300

    Creando Proyecto Dapper

---
+++ src/Cine.Core/IAdo.cs
@@ -0,0 +1,7 @@
+namespace Cine.Core;
+
+public interface IAdo
+{
+    public void AltaSala(Sala sala);
+    public List<Sala> ObtenerSalas();
+}
---
+++ src/Cine.Dapper/AdoDapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using MySqlConnector;
+using Cine.Core;
+
+namespace Cine.Dapper;
+public class AdoDapper : IAdo
+{
+    private readonly IDbConnection _conexion;
+    public AdoDapper(IDbConnection conexion) => this._conexion = conexion;
+    public AdoDapper(string cadena) => _conexion = new MySqlConnection(cadena);
+    
+    public void AltaSala(Sala sala)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Sala> ObtenerSalas()
+    {
+        throw new NotImplementedException();
+    }
+}
